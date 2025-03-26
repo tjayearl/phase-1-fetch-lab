a4e8901d9a05e33fe8edfbfebdf5c49241a5b302
tjayearl <iamtjayearl@gmail.com>
2025-03-26 23:03:31 +0300

    Add fetch lab files

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+    // To pass the tests, don't forget to return your fetch!
+    return fetch("https://anapioficeandfire.com/api/books")
+      .then((response) => response.json()) // Convert response to JSON
+      .then((books) => renderBooks(books)) // Pass books data to renderBooks()
+      .catch((error) => console.error("Error fetching books:", error)); // Handle errors
+  }
   
-}
-
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+  function renderBooks(books) {
+    const main = document.querySelector('main');
+    books.forEach(book => {
+      const h2 = document.createElement('h2');
+      h2.innerHTML = book.name;
+      main.appendChild(h2);
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    fetchBooks();
   });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+  
